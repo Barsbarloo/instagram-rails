6ffb2dc82ef18d91024dd76db0f9bc35450c7e36
Barsbarloo <lootanke@gmail.com>
2023-08-24 17:39:52 +0600

    add def login to model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }
+
+  attr_writer :login
+  def login
+    @login || self.username || self.email
+  end
 end
