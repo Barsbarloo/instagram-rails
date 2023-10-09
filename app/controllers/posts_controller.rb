a7e00bd99c36cc0f3cbc68a8db3e3a803b331e3d
Barsbarloo <lootanke@gmail.com>
2023-10-09 16:57:23 +0600

    add create methods to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,21 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @post = current_user.posts.new(post_params)
+
+    if @post.save 
+      #success stuff
+      redirect_to root_path
+    else
+      #failure stuff
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:photo)
+  end
 end
