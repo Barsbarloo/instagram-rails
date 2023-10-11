2c3f1e871b6d7131e4587b09067033fe80f0d7dd
Barsbarloo <lootanke@gmail.com>
2023-10-11 15:00:07 +0600

    db:migrate

--- db/migrate/20231011085615_add_status_to_posts.rb
+++ db/migrate/20231011085615_add_status_to_posts.rb
@@ -1,5 +1,5 @@
 class AddStatusToPosts < ActiveRecord::Migration[7.0]
   def change
-    add_column :posts, :status, :integer
+    add_column :posts, :status, :integer, null: false, default: 0
   end
 end
